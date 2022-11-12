dfcd534a51037ad9c3f2baf2b2f81c3a42796479
MohamedSalimJemai <mohamedsalim.jemai@esprit.tn>
2022-11-12 16:32:05 +0100

    final version test

--- src/test/java/tn/esprit/rh/achat/services/CategorieProduitServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/CategorieProduitServiceImplTest.java
@@ -1,5 +1,12 @@
 package tn.esprit.rh.achat.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
@@ -33,7 +40,44 @@ class CategorieProduitServiceImplTest {
 		categorieservice.retrieveCategorieProduit(1L);
 		Assertions.assertNotNull(categorieproduit);
 		System.out.println(categorieproduit);
-		System.out.println("Succes");
+		System.out.println("catégorie de produit valide");
+	}
+	
+	@Test
+	void addCategorieProduitTest() {
+		CategorieProduit categorieproduit10 = new CategorieProduit(10L,"qs","qsd",null);
+		categorieproduit10.setIdCategorieProduit(10L);
+		categorieservice.addCategorieProduit(categorieproduit10);
+		verify(categorieproduitrepository, times(1)).save(categorieproduit10);
+		System.out.println(categorieproduit10); 
+		System.out.println("Succées de création d'une catégorie de produit ");
+	}
+	
+	@Test 
+	void deleteCategorieProduitTest() {
+		CategorieProduit categorieproduit20 = new CategorieProduit(null,"qs","qsd",null);
+		categorieproduit20.setIdCategorieProduit(20L);
+		Mockito.lenient().when(categorieproduitrepository.findById(categorieproduit20.getIdCategorieProduit())).thenReturn(Optional.of(categorieproduit20));
+		categorieservice.deleteCategorieProduit(20L);
+		verify(categorieproduitrepository).deleteById(categorieproduit20.getIdCategorieProduit());
+		System.out.println(categorieproduit20);
+		System.out.println("Succées d'éffacement d'une catégorie de produit");
+	}
+	
+	@Test 
+	void retrieveAllCategorieProduits() {
+		List<CategorieProduit> categorieproduitList = new ArrayList<CategorieProduit>(){{
+			
+			add(new CategorieProduit(100L,"WWW","VVVV",null));
+			add(new CategorieProduit(200L,"KKKK","llll",null));
+			add(new CategorieProduit(300L,"uuuu","MMMM",null));
+
+		}};
+		
+		when(categorieservice.retrieveAllCategorieProduits()).thenReturn(categorieproduitList);
+		List<CategorieProduit> cpList = categorieservice.retrieveAllCategorieProduits();
+		assertEquals(3, cpList.size());
+		System.out.println("la fonction tourne correctement");
 	}
 
 }
